d13734e4eec4b0d2f350a88a5bd6ee3c26f29e23
strawberry <dragon60066@163.com>
2019-10-25 11:23:09 +0800

    add footer in pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Navbar/>
-    <nuxt/>
+    <nuxt/><br>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/navbar.vue';
+import Footer from '~/components/footer.vue'
 export default {
   components: {
-    Navbar
-  }  
+    Navbar,
+    Footer
+  }
 }
 </script>
