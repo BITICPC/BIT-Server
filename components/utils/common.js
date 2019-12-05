c986bbb392793e7a485788fe1c94a089b9aa0969
Shuibin Long <dragon60066@163.com>
2019-12-05 16:14:18 +0800

    Fixed a bug about calculating the time difference

--- components/utils/common.js
+++ components/utils/common.js
@@ -7,28 +7,28 @@ export default {
   getTimeDifferent (lastTime) {
     const dateLast = moment(lastTime)
     const dateNow = moment(new Date())
-    if (dateLast.isAfter(dateNow)) {
-      return 'Invalid'
+    if (dateNow.isSameOrAfter(dateNow)) {
+      const minutes = dateNow.diff(dateLast, 'minute')
+      if (minutes < 1) {
+        return '刚刚'
+      } else if (minutes < 60) {
+        return minutes + ' 分钟前'
+      }
+      const hours = Math.floor(minutes / 60)
+      if (hours < 24) {
+        return hours + ' 小时前'
+      }
+      const days = Math.floor(hours / 24)
+      if (days < 30) {
+        return days + ' 天前'
+      }
+      const months = Math.floor(days / 30)
+      if (months < 12) {
+        return months + ' 个月前'
+      }
+      const years = Math.floor(months / 12)
+      return years + ' 年前'
     }
-    const minutes = dateNow.diff(dateLast, 'minute')
-    if (minutes < 1) {
-      return '刚刚'
-    } else if (minutes < 60) {
-      return minutes + ' 分钟前'
-    }
-    const hours = Math.floor(minutes / 60)
-    if (hours < 24) {
-      return hours + ' 小时前'
-    }
-    const days = Math.floor(hours / 24)
-    if (days < 30) {
-      return days + ' 天前'
-    }
-    const months = Math.floor(days / 30)
-    if (months < 12) {
-      return months + ' 个月前'
-    }
-    const years = Math.floor(months / 12)
-    return years + ' 年前'
+    return 'Invalid'
   }
 }
