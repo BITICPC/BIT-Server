e5a7f7e5d950da1a146687fc301d6915909ea80e
Shuibin Long <dragon60066@163.com>
2019-12-13 23:40:25 +0800

    elint error fixed

--- pages/user/_id/index.vue
+++ pages/user/_id/index.vue
@@ -31,7 +31,7 @@
             <v-img :src="avatar" :lazy-src="avatar">
               <template v-slot:placeholder>
                 <v-row class="fill-height ma-0" align="center" justify="center">
-                    <v-progress-circular indeterminate color="grey lighten-5" />
+                  <v-progress-circular indeterminate color="grey lighten-5" />
                 </v-row>
               </template>
             </v-img>
